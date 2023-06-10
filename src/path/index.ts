49c7d7991ce74b74d37630c32bb32d361866152e
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-06-10 16:44:39 +0200

    remove: joinPath, resolvePath, toRelativePath
    Use instead:
     - joinPath => path.join
     - resolvePath => path.resolve
     - toRelativePath => path.relative (use `process.cwd()` as first arg)

--- src/path/index.ts
+++ src/path/index.ts
@@ -1,4 +1,1 @@
 export { getFileName } from "./getFileName";
-export { joinPath } from "./joinPath";
-export { resolvePath } from "./resolvePath";
-export { toRelativePath } from "./toRelativePath";
--- src/path/joinPath.ts
+++
@@ -1,5 +0,0 @@
-import { join } from "path";
-
-export function joinPath(...pieces: string[]) {
-  return join(...pieces);
-}
